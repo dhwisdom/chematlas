73dcc0a0343044d6d22813cc69a9bd00892c7663
Dalton Wisdom <dhwisdom@uark.edu>
2026-09-10 16:10:22 -0500

    Connect ChemAtlas to dedicated Supabase project

--- data/platform-config.js
+++ data/platform-config.js
@@ -1,6 +1,6 @@
 window.CHEMATLAS_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: '',
+  supabaseUrl: 'https://zslxoqvqxdqwykzdogor.supabase.co',
+  supabasePublishableKey: 'sb_publishable_CQnGVuXMrI_wR6cbCcTkDA_dbk5wOD8',
   tutorEndpoint: '/api/tutor',
   canonicalHost: 'chematlas-red.vercel.app'
 };
